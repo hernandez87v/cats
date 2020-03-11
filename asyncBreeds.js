940ab1841eb30154bf8bc733824ba9e5aa563fa1
Vlad <hernandez87v@hotmail.com>
2020-03-11 19:17:36 +0000

    Adding updated cat excercise

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -6,12 +6,13 @@ const breedDetailsFromFile = function(breed) {
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     console.log("In readFile's Callback: it has the data.");
     // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
-    if (!error) return data;
+    if (!error) callback(data);
   });
   // ISSUE: Attempting to return data out here will also not work.
   //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
 };
 
-// we try to get the return value
+// // we try to get the return value
 const bombay = breedDetailsFromFile('Bombay');
-console.log('Return Value: ', bombay); // => will NOT print out details, instead we will see undefined!
+// console.log('Return Value: ', bombay); // => will NOT print out details, instead we will see undefined!
+module.exports = breedDetailsFromFile;
